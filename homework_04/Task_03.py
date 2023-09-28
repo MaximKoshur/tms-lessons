b9f9cf6a9cca602ffbe9e66be5fa1117eb4eb5ec
Maxim Koshur <koshur.max@gmail.com>
2023-09-28 12:41:42 +0300

    homework

--- homework_04/Task_03.py
+++ homework_04/Task_03.py
@@ -1,15 +1,15 @@
-import random
-
-print(random.randint(0,100))
-while True:
-
-    a = input("Should we break? ")
-    if a =='yes':
-        break
-    elif a == 'no':
-        print(random.randint(0, 100))
-    else:
-        print('Dont understand you')
+a=0
+for i in range(0,101):
+    if a == 'yes':break
+    print(i)
+    while True:
+        a = input("Should we break? ")
+        if a =='yes':
+            break
+        elif a == 'no':
+            break
+        else:
+            print('Dont understand you')
 
 
 
